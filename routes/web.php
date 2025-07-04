85ac0dc7e3b6334221f986237b31b1d4006c5edd
KhamHsai <khamhsai2004@gmail.com>
2025-07-04 20:37:56 +0700

    [Update]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello Laravel' ;
 });
+
+Route::get('/welcome', [PageController::class, 'welcome']);
+Route::get('/home', [HomeController::class, 'home']);
